6d09e1b53d23fa5ff4eeb19835c971feb4e7e9ad
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-15 19:36:43 -0400

    skeleton layed out

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <p>
+      <a href="https://github.com/StephenSaylor">Github Profile</a>
+        <a href="https://stephensaylor.github.io/horiseon-seo-optimization/">Horiseon Landing Page</a>
+        <a href="https://stephensaylor.github.io/password-generator/">Random Password Generator</a>
+        <a href="https://stephensaylor.github.io/Weather-Dashboard/">Weather Dashboard</a>
+        <a href="https://franklincodes1.github.io/GroupProject1/">On the Way (app idea)</a>
+        <a href="#Linked in">Linked in placeholder</a>
+        <a href="#Resume">Resume placeholder</a>
+      </p>
+    </div>
+  );
+}

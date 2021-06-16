4d2a284c684a4a3cf4a415923ef387966bada44f
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-15 20:00:13 -0400

    listed portfolio links

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,15 +4,29 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
-      <p>
-      <a href="https://github.com/StephenSaylor">Github Profile</a>
-        <a href="https://stephensaylor.github.io/horiseon-seo-optimization/">Horiseon Landing Page</a>
-        <a href="https://stephensaylor.github.io/password-generator/">Random Password Generator</a>
-        <a href="https://stephensaylor.github.io/Weather-Dashboard/">Weather Dashboard</a>
-        <a href="https://franklincodes1.github.io/GroupProject1/">On the Way (app idea)</a>
-        <a href="#Linked in">Linked in placeholder</a>
-        <a href="#Resume">Resume placeholder</a>
-      </p>
+      <ul>
+        <li>
+          <a href="https://github.com/StephenSaylor">Github Profile</a>
+        </li>
+        <li>
+          <a href="https://stephensaylor.github.io/horiseon-seo-optimization/">Horiseon Landing Page</a>
+        </li>
+        <li>
+          <a href="https://stephensaylor.github.io/password-generator/">Random Password Generator</a>
+        </li>  
+        <li>
+          <a href="https://stephensaylor.github.io/Weather-Dashboard/">Weather Dashboard</a>
+        </li>
+        <li>
+          <a href="https://franklincodes1.github.io/GroupProject1/">On the Way (app idea)</a>
+        </li>
+        <li> 
+          <a href="#Linked in">Linked in placeholder</a>
+        </li>
+        <li>
+          <a href="#Resume">Resume placeholder</a>
+        </li>  
+      </ul>
     </div>
   );
 }

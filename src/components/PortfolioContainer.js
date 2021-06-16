2716ce37916d7c5cd4c4b1aa2465c9e259411aad
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-16 19:50:24 -0400

    added footer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Footer from './Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -26,6 +27,7 @@ export default function PortfolioContainer() {
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
+      <Footer />
     </div>
   );
 }

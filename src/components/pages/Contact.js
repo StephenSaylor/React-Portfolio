319589f8b98faee821dc1e6563a0682fa0ca2c72
Jeff Lebowski <anentitymusic@gmail.com>
2021-06-16 20:23:41 -0400

    added contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,6 +7,21 @@ export default function Contact() {
       <p>
       Email: anentitymusic@gmail.com
       </p>
+      <form id="contact-form">
+          <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="form-control" id="name" />
+          </div>
+          <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="email"  />
+          </div>
+          <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" rows="5" id="message"  />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     </div>
   );
 }
